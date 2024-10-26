d7f411df1c4cdfdb4e9588b8be8785ca1575b7e2
bego46 <berlad46@gmail.com>
2024-10-25 19:41:47 -0500

    Ejercicios de C5 Completados

--- C5 Fundamentos Javascript/ejercicios/18.js
+++ C5 Fundamentos Javascript/ejercicios/18.js
@@ -2,12 +2,18 @@ function esVocal(letra) {
   // La función recibe un argumento llamado letra el cual es un string.
   // Retorna como resultado un string que indica si el argumento letra es una vocal.
   // de lo contrario retorna el string "Dato incorrecto"
-  // Por ejemplo: 
+  // Por ejemplo:
   // "a" ---> "Es vocal"
   // "u" ---> "Es vocal"
   // "n" ---> "Dato incorrecto"
   // "texto largo" ---> "Dato incorrecto"
   // Tu código:
+  str1 = "aeiouAEIOU";
+  if (str1.includes(letra)) {
+    return "Es vocal";
+  } else {
+    return "Dato incorrecto";
+  }
 }
 
 module.exports = esVocal;
